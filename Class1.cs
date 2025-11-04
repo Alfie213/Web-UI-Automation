7a615d0cab4391d5204b0d621370caf39d579abd
Alfie213 <arturbogoratov@gmail.com>
2025-11-04 18:11:49 +0400

    NUnit

--- Class1.cs
+++ Class1.cs
@@ -2,14 +2,21 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium.Interactions;
 
-namespace Web_UI_Automation
+namespace Web_UI_Automation.NUnit
 {
+    // d. Category
     [TestFixture]
+    [Category("EHU_Website")]
     public class Class1
     {
         private IWebDriver _driver;
 
+        // b. Test setup
         [SetUp]
         public void Setup()
         {
@@ -19,57 +26,72 @@ namespace Web_UI_Automation
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
+        // b. Test tear down
         [TearDown]
         public void TearDown()
         {
             _driver.Quit();
         }
 
+        // d. Category
         [Test]
+        [Category("Navigation")]
         public void Test_AboutEhuPageNavigation()
         {
             _driver.Navigate().GoToUrl("https://en.ehu.lt/");
             var aboutLink = _driver.FindElements(By.XPath("//a[contains(text(), 'About')]")).FirstOrDefault();
-            Assert.That(aboutLink, Is.Not.Null, "About EHU link not found.");
+            Assert.That(aboutLink, Is.Not.Null);
             aboutLink.Click();
 
             Assert.That(_driver.Url, Does.Contain("https://en.ehuniversity.lt/about/"));
             Assert.That(_driver.Title, Does.Contain("About"));
 
             var header = _driver.FindElement(By.XPath("//strong[contains(text(), 'European Humanities University (EHU)')]"));
-            Assert.That(header, Is.Not.Null, "Header text not found on About EHU page.");
+            Assert.That(header, Is.Not.Null);
         }
 
+        // c. Data provider (TestCaseSource)
+        public static IEnumerable<TestCaseData> SearchTermsData()
+        {
+            yield return new TestCaseData("study programs", "/?s=study+programs", "study");
+            yield return new TestCaseData("admission", "/?s=admission", "admission");
+        }
+
+        // d. Category
         [Test]
-        public void Test_SearchFunctionality()
+        [Category("Search")]
+        [TestCaseSource(nameof(SearchTermsData))]
+        public void Test_SearchFunctionality(string searchTerm, string expectedUrlPart, string expectedResultWord)
         {
             _driver.Navigate().GoToUrl("https://en.ehu.lt/");
 
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
 
             var searchContainer = wait.Until(d => d.FindElement(By.CssSelector(".header-search")));
-            var actions = new OpenQA.Selenium.Interactions.Actions(_driver);
+            var actions = new Actions(_driver);
             actions.MoveToElement(searchContainer).Perform();
 
             var searchInput = wait.Until(d => d.FindElement(By.CssSelector("input[name='s']")));
-            Assert.That(searchInput, Is.Not.Null, "Search input not found.");
+            Assert.That(searchInput, Is.Not.Null);
 
-            searchInput.SendKeys("study programs");
+            searchInput.SendKeys(searchTerm);
 
             var searchButton = _driver.FindElement(By.CssSelector(".header-search__form button[type='submit']"));
-            Assert.That(searchButton, Is.Not.Null, "Search button not found.");
+            Assert.That(searchButton, Is.Not.Null);
 
             searchButton.Click();
 
-            wait.Until(d => d.Url.Contains("/?s=study+programs"));
-            Assert.That(_driver.Url, Does.Contain("/?s=study+programs"));
+            wait.Until(d => d.Url.Contains(expectedUrlPart));
+            Assert.That(_driver.Url, Does.Contain(expectedUrlPart));
 
             var results = _driver.FindElements(By.CssSelector("article a"));
-            Assert.That(results.Any(r => r.Text.ToLower().Contains("study")), Is.True, "No relevant search results found.");
+            Assert.That(results.Any(r => r.Text.ToLower().Contains(expectedResultWord)), Is.True);
         }
 
 
+        // d. Category
         [Test]
+        [Category("Navigation")]
         public void Test_LanguageChangeToLithuanian()
         {
             _driver.Navigate().GoToUrl("https://en.ehu.lt/");
@@ -77,7 +99,7 @@ namespace Web_UI_Automation
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
 
             var langSwitcherMain = wait.Until(d => d.FindElement(By.CssSelector(".language-switcher > li > a")));
-            var actions = new OpenQA.Selenium.Interactions.Actions(_driver);
+            var actions = new Actions(_driver);
             actions.MoveToElement(langSwitcherMain).Perform();
 
             var lithuanianLink = wait.Until(d => d.FindElement(By.CssSelector(".language-switcher li ul li a[href='https://lt.ehuniversity.lt/']")));
@@ -87,11 +109,12 @@ namespace Web_UI_Automation
             Assert.That(_driver.Url, Does.Contain("https://lt.ehuniversity.lt/"));
 
             var bodyText = _driver.FindElement(By.TagName("body")).Text;
-            Assert.That(bodyText.Contains("Apie"), Is.True, "Lithuanian content not detected.");
+            Assert.That(bodyText.Contains("Apie"), Is.True);
         }
 
-
+        // d. Category
         [Test]
+        [Category("Contact")]
         public void Test_ContactInfoDisplayed()
         {
             _driver.Navigate().GoToUrl("https://en.ehu.lt/contact/");
